628407cf58d94c0988728a692ca826d0a2e4b797
jalsa <jon_asdo@hotmail.com>
2014-04-23 10:13:11 +0200

    Ejercicio para calcular números primos.

--- EjerciciosJava/src/com/jon/funciones/EJ8_22.java
+++ EjerciciosJava/src/com/jon/funciones/EJ8_22.java
@@ -9,35 +9,10 @@ public class EJ8_22 {
 		int numero;
 		Scanner entrada = new Scanner(System.in);
 		numero = entrada.nextInt();
-		Vector<Integer> array = listaDePrimos(numero);
+		Primos p = new Primos(numero);
+		Vector<Integer> array = p.listaDePrimos();
 		for (int j=0; j<array.size(); j++) {
 			System.out.println(array.get(j));
 		}
-	}
-	public static boolean esPrimo(int x) {
-		int divisor = 2;
-		boolean primo = true;
-		if (x == 1 || x == 2) {
-			primo = false;
-		}
-		while(primo && divisor<x) {
-			if (x % divisor == 0) {
-				primo = false;
-			}
-			else {
-				divisor++;
-			}
-		}
-		return primo;
-	}
-	public static Vector<Integer> listaDePrimos(int numero) {
-		Vector<Integer> v = new Vector<Integer>();
-		for (int i=1; i<=numero; i++) {
-			if (esPrimo(i)) {
-				v.add(new Integer(i));
-			}
-		}
-		//Integer[] resultado = (Integer[]) v.toArray(); 
-		return v;
 	}
 }
---
+++ EjerciciosJava/src/com/jon/funciones/Primos.java
@@ -0,0 +1,36 @@
+package com.jon.funciones;
+
+import java.util.Vector;
+
+public class Primos {
+
+	private int x;
+	public Primos(int x) {
+		this.x = x;
+	}
+	public Vector<Integer> listaDePrimos() {
+		Vector<Integer> v = new Vector<Integer>();
+		for (int i=1; i<=this.x; i++) {
+			if (esPrimo(i)) {
+				v.add(new Integer(i));
+			}
+		}
+		return v;
+	}
+	public boolean esPrimo(int x) {
+		int divisor = 2;
+		boolean primo = true;
+		if (x == 1 || x == 2) {
+			primo = false;
+		}
+		while(primo && divisor<x) {
+			if (x % divisor == 0) {
+				primo = false;
+			}
+			else {
+				divisor++;
+			}
+		}
+		return primo;
+	}
+}
