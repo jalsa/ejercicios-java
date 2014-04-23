8be7b0dbd4cac586e603908eb7e2e51401357298
jalsa <jon_asdo@hotmail.com>
2014-04-23 13:31:12 +0200

    Ejercicio para validar un DNI.

--- EjerciciosJava/src/com/jon/clases/ValidarDni.java
+++ EjerciciosJava/src/com/jon/clases/ValidarDni.java
@@ -9,6 +9,7 @@ public class ValidarDni {
 		boolean valido = true;
 		char[] cArray, cArray1;
 		char c;
+		char[] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'};
 		if (this.nif.length() != 9) {
 			valido = false;
 		}
@@ -25,6 +26,13 @@ public class ValidarDni {
 			cArray1 = letra.toCharArray();
 			if (!Character.isLetter(cArray1[0])) {
 				valido = false;
+			}
+			else {
+				int resto = parseInt(cadena) % 23;
+	            char letraDni = letras[resto];
+	            if (letraDni != letra) {
+	            	valido = false;
+	            }
 			}
 		}
 		return valido;
