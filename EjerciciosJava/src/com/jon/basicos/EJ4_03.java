c403b0b89577561489f8ffa22d5861ead853ce28
jalsa <jon_asdo@hotmail.com>
2014-04-23 08:45:17 +0200

    Ejercicio de cálculo de volumen y superficie de una esfera.

---
+++ EjerciciosJava/src/com/jon/basicos/EJ4_03.java
@@ -0,0 +1,19 @@
+package com.jon.basicos;
+
+import java.util.Scanner;
+
+public class EJ4_03 {
+	public static void main(String[] args) {
+		int radio;
+		double volumen, superficie;
+		
+		Scanner entrada = new Scanner(System.in);
+		radio = entrada.nextInt();
+		
+		volumen = (4/3) * Math.PI * Math.pow(radio, 3);
+		superficie = 4 * Math.PI * Math.pow(radio, 2);
+		
+		System.out.println("El volumen de la esfera es: " + volumen);
+		System.out.println("La superficie de la esfera es: " + superficie);
+	}
+}
