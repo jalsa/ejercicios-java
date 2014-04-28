88bd84433df9ff3f7f830e234f4d4c5efd9d2766
jalsa <jon_asdo@hotmail.com>
2014-04-28 12:31:56 +0200

    Añadido menú de juego en los tests.

--- PractivaJava/src/com/jon/tests/JuegoTest.java
+++ PractivaJava/src/com/jon/tests/JuegoTest.java
@@ -3,6 +3,7 @@ package com.jon.tests;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,6 +21,8 @@ public class JuegoTest {
 	JuegoAdivinaPar juegoP;
 	JuegoAdivinaImpar juegoI;
 	ArrayList<Jugable> juegos;
+	int numero, respuesta;
+	boolean repetir = true;
 	
 	@Before
 	public void setUp() {
@@ -45,15 +48,28 @@ public class JuegoTest {
 		assertEquals("Juego 1", 5, juego.obtenerVidasRestantes());
 		juego.actualizaRecord();
 		juego1.actualizaRecord();*/
-		juegos.get(0).muestraNombre();
-		juegos.get(0).muestraInfo();
-		juegos.get(0).juega();
-		juegos.get(1).muestraNombre();
-		juegos.get(1).muestraInfo();
-		juegos.get(1).juega();
-		juegos.get(2).muestraNombre();
-		juegos.get(2).muestraInfo();
-		juegos.get(2).juega();
+		while (repetir) {
+			System.out.println("Elije uno de estos juegos:\n");
+			System.out.println("0: Adivinar un numero\n");
+			System.out.println("1: Adivinar un numero par\n");
+			System.out.println("2: Adivinar un numero impar\n");
+			Scanner entrada = new Scanner(System.in);
+			numero = entrada.nextInt();
+			while (numero != 0 && numero != 1 && numero != 2) {
+				entrada = new Scanner(System.in);
+				numero = entrada.nextInt();
+			}
+			juegos.get(numero).muestraNombre();
+			juegos.get(numero).muestraInfo();
+			juegos.get(numero).juega();
+			System.out.println("ÀDesea jugar de nuevo?\n0: S’\n1: No");
+			entrada = new Scanner(System.in);
+			respuesta = entrada.nextInt();
+			if (respuesta == 1) {
+				repetir = false;
+			}
+		}
+		
 	}
 
 }
