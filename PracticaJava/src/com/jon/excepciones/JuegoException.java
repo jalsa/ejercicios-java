fe6256d5cc21616372981cdc41be653ed55be0b5
jalsa <jon_asdo@hotmail.com>
2014-04-28 19:16:56 +0200

    Añadida una excepción si se introduce un número no válido.

---
+++ PracticaJava/src/com/jon/excepciones/JuegoException.java
@@ -0,0 +1,10 @@
+package com.jon.excepciones;
+
+public class JuegoException extends Exception {
+
+	private static final long serialVersionUID = -1344582094207083962L;
+
+	public JuegoException(String mensaje) {
+		super(mensaje);
+	}
+}
--- PracticaJava/src/com/jon/tests/JuegoTest.java
+++ PracticaJava/src/com/jon/tests/JuegoTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Vector;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +13,7 @@ import com.jon.clases.Juego;
 import com.jon.clases.JuegoAdivinaImpar;
 import com.jon.clases.JuegoAdivinaNumero;
 import com.jon.clases.JuegoAdivinaPar;
+import com.jon.excepciones.JuegoException;
 import com.jon.interfaces.Jugable;
 
 public class JuegoTest {
@@ -20,41 +22,65 @@ public class JuegoTest {
 	static JuegoAdivinaNumero juegoA;
 	static JuegoAdivinaPar juegoP;
 	static JuegoAdivinaImpar juegoI;
-	static ArrayList<Jugable> juegos;
+	static Vector<Jugable> juegos;
 	Jugable j;
 	static int numero;
 	static int primero = 1;
 	int respuesta;
 	static boolean repetir = true;
+	static Scanner entrada;
+	static int contador = 0;
 	
 	@Before
 	public void setUp() {
-		j = elijeJuego();
+		try {
+			j = elijeJuego();
+		} catch (JuegoException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
-	public static Jugable elijeJuego() {
-		System.out.println(primero);
+	protected static void validateException(int num) throws JuegoException {
+		if (num != 0 && num != 1 && num != 2) {
+			throw new JuegoException("Nœmero no v‡lido: debe de ser entre 0 y 2");
+		}
+	}
+	
+	public static Jugable elijeJuego() throws JuegoException {
 		if (primero == 1) {
-			juegos = new ArrayList<Jugable>(3);
+			juegos = new Vector<Jugable>(3);
+			infoVector(juegos);
 			juegoA = new JuegoAdivinaNumero(3);
 			juegoP = new JuegoAdivinaPar(3);
 			juegoI = new JuegoAdivinaImpar(3);
 			juegos.add(juegoA);
 			juegos.add(juegoP);
 			juegos.add(juegoI);
+			infoVector(juegos);
 			primero = 0;
 		}
 		System.out.println("Elije uno de estos juegos:\n");
 		System.out.println("0: Adivinar un numero\n");
 		System.out.println("1: Adivinar un numero par\n");
 		System.out.println("2: Adivinar un numero impar\n");
-		Scanner entrada = new Scanner(System.in);
+		entrada = new Scanner(System.in);
 		numero = entrada.nextInt();
 		while (numero != 0 && numero != 1 && numero != 2) {
+			validateException(numero);
 			entrada = new Scanner(System.in);
 			numero = entrada.nextInt();
 		}
 		return juegos.get(numero);
+	}
+	
+	public static void infoVector(Vector<Jugable> v) {
+		for (int i=0; i<v.size(); i++) {
+			if (v.get(i) != null) {
+				contador++;
+			}
+		}
+		System.out.println("Capacidad del vector: " + v.size() + " y tamaño: " + contador);
+		contador = 0;
 	}
 	
 	@Test
@@ -72,7 +98,7 @@ public class JuegoTest {
 		j.muestraInfo();
 		j.juega();
 		System.out.println("ÀDesea jugar de nuevo?\n0: S’\n1: No");
-		Scanner entrada = new Scanner(System.in);
+		entrada = new Scanner(System.in);
 		respuesta = entrada.nextInt();
 		if (respuesta == 0) {
 			setUp();
