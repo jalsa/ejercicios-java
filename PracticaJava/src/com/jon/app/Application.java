4c23d2ba25e349d8246d2b3ca6895f3a42ffb78d
jalsa <jon_asdo@hotmail.com>
2014-04-29 09:27:47 +0200

    Excepción y Clase Application añadidas.

---
+++ PracticaJava/src/com/jon/app/Application.java
@@ -0,0 +1,71 @@
+package com.jon.app;
+
+import java.util.Scanner;
+import java.util.Vector;
+
+import com.jon.clases.JuegoAdivinaImpar;
+import com.jon.clases.JuegoAdivinaNumero;
+import com.jon.clases.JuegoAdivinaPar;
+import com.jon.excepciones.JuegoException;
+import com.jon.interfaces.Jugable;
+
+public class Application {
+
+	JuegoAdivinaNumero juegoA;
+	JuegoAdivinaPar juegoP;
+	JuegoAdivinaImpar juegoI;
+	Vector<Jugable> juegos;
+	int numero;
+	int primero = 1;
+	boolean repetir = true;
+	Scanner entrada;
+	int contador = 0;
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	protected void validateException(int num) throws JuegoException {
+		if (num != 0 && num != 1 && num != 2) {
+			throw new JuegoException("Nœmero no v‡lido: debe de ser entre 0 y 2");
+		}
+	}
+	
+	public void infoVector(Vector<Jugable> v) {
+		for (int i=0; i<v.size(); i++) {
+			if (v.get(i) != null) {
+				contador++;
+			}
+		}
+		System.out.println("Capacidad del vector: " + v.size() + " y tamaño: " + contador);
+		contador = 0;
+	}
+	
+	public Jugable elijeJuego() throws JuegoException {
+		if (primero == 1) {
+			juegos = new Vector<Jugable>(3);
+			infoVector(juegos);
+			juegoA = new JuegoAdivinaNumero(3);
+			juegoP = new JuegoAdivinaPar(3);
+			juegoI = new JuegoAdivinaImpar(3);
+			juegos.add(juegoA);
+			juegos.add(juegoP);
+			juegos.add(juegoI);
+			infoVector(juegos);
+			primero = 0;
+		}
+		System.out.println("Elije uno de estos juegos:\n");
+		System.out.println("0: Adivinar un numero\n");
+		System.out.println("1: Adivinar un numero par\n");
+		System.out.println("2: Adivinar un numero impar\n");
+		entrada = new Scanner(System.in);
+		numero = entrada.nextInt();
+		while (numero != 0 && numero != 1 && numero != 2) {
+			validateException(numero);
+			entrada = new Scanner(System.in);
+			numero = entrada.nextInt();
+		}
+		return juegos.get(numero);
+	}
+	
+}
--- PracticaJava/src/com/jon/tests/JuegoTest.java
+++ PracticaJava/src/com/jon/tests/JuegoTest.java
@@ -9,6 +9,7 @@ import java.util.Vector;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.jon.app.Application;
 import com.jon.clases.Juego;
 import com.jon.clases.JuegoAdivinaImpar;
 import com.jon.clases.JuegoAdivinaNumero;
@@ -18,82 +19,27 @@ import com.jon.interfaces.Jugable;
 
 public class JuegoTest {
 
+	Application app;
 	Juego juego, juego1;
-	static JuegoAdivinaNumero juegoA;
-	static JuegoAdivinaPar juegoP;
-	static JuegoAdivinaImpar juegoI;
-	static Vector<Jugable> juegos;
 	Jugable j;
-	static int numero;
-	static int primero = 1;
 	int respuesta;
-	static boolean repetir = true;
-	static Scanner entrada;
-	static int contador = 0;
+	Scanner entrada;
 	
 	@Before
 	public void setUp() {
-		try {
-			j = elijeJuego();
-		} catch (JuegoException e) {
-			System.out.println(e.getMessage());
-		}
-	}
-	
-	protected static void validateException(int num) throws JuegoException {
-		if (num != 0 && num != 1 && num != 2) {
-			throw new JuegoException("Nœmero no v‡lido: debe de ser entre 0 y 2");
-		}
-	}
-	
-	public static Jugable elijeJuego() throws JuegoException {
-		if (primero == 1) {
-			juegos = new Vector<Jugable>(3);
-			infoVector(juegos);
-			juegoA = new JuegoAdivinaNumero(3);
-			juegoP = new JuegoAdivinaPar(3);
-			juegoI = new JuegoAdivinaImpar(3);
-			juegos.add(juegoA);
-			juegos.add(juegoP);
-			juegos.add(juegoI);
-			infoVector(juegos);
-			primero = 0;
-		}
-		System.out.println("Elije uno de estos juegos:\n");
-		System.out.println("0: Adivinar un numero\n");
-		System.out.println("1: Adivinar un numero par\n");
-		System.out.println("2: Adivinar un numero impar\n");
-		entrada = new Scanner(System.in);
-		numero = entrada.nextInt();
-		while (numero != 0 && numero != 1 && numero != 2) {
-			validateException(numero);
-			entrada = new Scanner(System.in);
-			numero = entrada.nextInt();
-		}
-		return juegos.get(numero);
-	}
-	
-	public static void infoVector(Vector<Jugable> v) {
-		for (int i=0; i<v.size(); i++) {
-			if (v.get(i) != null) {
-				contador++;
+		app = new Application();
+		j = null;
+		while (j == null) {
+			try {
+				j = app.elijeJuego();
+			} catch (JuegoException e) {
+				System.out.println(e.getMessage());
 			}
 		}
-		System.out.println("Capacidad del vector: " + v.size() + " y tamaño: " + contador);
-		contador = 0;
 	}
 	
 	@Test
 	public void test() {
-		/*assertEquals("Juego 1", 5, juego.obtenerVidasRestantes());
-		assertEquals("Juego 1", true, juego.quitaVida());
-		assertEquals("Juego 1", 4, juego.obtenerVidasRestantes());
-		assertEquals("Juego 2", 5, juego1.obtenerVidasRestantes());
-		assertEquals("Juego 1", 4, juego.obtenerVidasRestantes());
-		juego.reiniciaPartida();
-		assertEquals("Juego 1", 5, juego.obtenerVidasRestantes());
-		juego.actualizaRecord();
-		juego1.actualizaRecord();*/
 		j.muestraNombre();
 		j.muestraInfo();
 		j.juega();
